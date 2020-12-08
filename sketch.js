4f12cc1a3cbc160a3194a6f851bc38b1d46fd82e
AthivaaHarlalka <73229861+AthivaaHarlalka@users.noreply.github.com>
2020-12-08 21:42:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var magicalball;
+var database,position;
+
+function setup(){
+    database=firebase.database();
+    console.log(database);
+    createCanvas(500,500);
+    magicalball = createSprite(250,250,10,10);
+    magicalball.shapeColor = "red";
+
+    var magicalballPosition=database.ref('ball/position');
+    magicalballPosition.on("value",readPosition,showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+         writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+database.ref('ball/position').set({
+    'x':position.x+x,
+    'y':position.y+y
+});
+    
+}
+
+
+function readPosition(data){
+    position=data.val();
+    console.log(position.x);
+    console.log(position.y);
+
+    magicalball.x=position.x;
+    magicalball.y=position.y;
+}
+
+function showError(){
+    console.log("write error");
+}
